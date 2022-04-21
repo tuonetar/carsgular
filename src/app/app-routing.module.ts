ff9dfb6ba17e87a15efa3cf950d2ac0a59e709d7
tuonetar <karina.martynava@gmail.com>
2022-04-21 09:09:09 +0200

    routing redirect

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { CarformComponent } from './carform/carform.component';
 import { CareditComponent } from './components/caredit/caredit.component';
 
 const routes: Routes = [
-  //{ path: '', redirectTo: 'carslist', pathMatch: 'full'},
+  { path: '', redirectTo: 'carslist', pathMatch: 'full'},
   { path: 'carslist', component: CarslistComponent },
   { path: 'add', component: CarformComponent},
   { path: 'edit/:id', component: CareditComponent}
